ed13216585497f9e62663d4ed1f976820299963d
archepro84 <archepro84@gmail.com>
2022-04-30 11:52:05 +0900

    Initial Commit

---
+++ jpa/src/main/java/prac/pracorm/PracOrmApplication.java
@@ -0,0 +1,13 @@
+package prac.pracorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracOrmApplication.class, args);
+	}
+
+}
---
+++ jpa/src/test/java/prac/pracorm/PracOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package prac.pracorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
