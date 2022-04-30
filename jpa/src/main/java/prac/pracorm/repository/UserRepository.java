2325370a35837cd443afb2da77d2c12a09bc620f
archepro84 <archepro84@gmail.com>
2022-04-30 23:08:50 +0900

    feat: Add User domain

---
+++ jpa/src/main/java/prac/pracorm/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package prac.pracorm.repository;
+
+import prac.pracorm.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+    User save(User user);
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByName(String name);
+
+    List<User> findAll();
+
+}
