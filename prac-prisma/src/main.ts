cee01801958da466ae0b95c495df8a4fb89b6531
Cobb Lee <archepro84@gmail.com>
2023-02-22 23:55:38 +0900

    feat: Add prisma example

--- prac-prisma/src/main.ts
+++ prac-prisma/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  await useNestLifeCycle(app);
+
   await app.listen(3000);
 }
+
+async function useNestLifeCycle(app: INestApplication): Promise<void> {
+  // Nest.js 생명주기에 맞춰 PrismaClient를 종료시키기 위해 설정합니다.
+  const prismaService = await app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+}
+
 bootstrap();
